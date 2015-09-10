e32ef964cacb0b6524edfd64de39237a7f99e3dd
oscar+ari <oscar+ari@aa.io>
2015-09-10 15:38:51 -0400

    refresh button working sorta

--- app/assets/javascripts/views/feed_show.js
+++ app/assets/javascripts/views/feed_show.js
@@ -1,18 +1,31 @@
 NewsReader.Views.FeedShow = Backbone.View.extend({
-  template: JST['feed_show_item'],
-
-  tagName: 'ul',
+  template: JST['feed_show'],
 
   initialize: function(){
     this.listenTo(this.model, 'sync', this.render);
   },
 
+  events: {
+    'click button.refresh': 'refreshPage'
+  },
+
   render: function() {
+    // var $button = $('<button class="refresh">Refresh</button>')
+    this.$el.empty();
+    var $ul = $('<ul>');
     var entries = this.model.entries();
     entries.each(function(entry) {
-      this.$el.append(this.template({ entry: entry }));
+      $ul.append(JST['feed_show_item']({ entry: entry }));
     }.bind(this));
+    var page = this.template({model: this.model})
+    this.$el.append(page).append($ul);
+
     return this;
+  },
+
+  refreshPage: function() {
+    console.log('refeshing')
+    this.model.fetch();
   }
 
 })
